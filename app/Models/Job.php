039bc9affd60c60c35b90c9bfc5740b5ef5a81db
ZicoBacker <150025315+ZicoBacker@users.noreply.github.com>
2024-09-30 14:01:41 +0200

    30-9-2024 Episode 10

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Arr;
 
 class Job extends Model
 {
+    use HasFactory, Notifiable;
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
