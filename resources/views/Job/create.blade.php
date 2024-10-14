a9c6386229133008e54ff1428cf846f1e523b8d0
ZicoBacker <150025315+ZicoBacker@users.noreply.github.com>
2024-10-14 13:12:55 +0200

    video 17 complete

--- resources/views/Job/create.blade.php
+++ resources/views/Job/create.blade.php
@@ -29,8 +29,11 @@
                         <label for="title" class="block text-sm font-medium leading-6 text-gray-900">title</label>
                         <div class="mt-2">
                             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Shift leader">
+                                <input type="text" name="title" id="title" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Shift leader" required>
                             </div>
+                            @error('title')
+                            <p class="text-xs text-red-500 font-semibold mt-1">{{$message}}</p>
+                            @enderror
                         </div>
                     </div>
 
@@ -38,10 +41,24 @@
                         <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
                         <div class="mt-2">
                             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="Salary" id="Salary" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 Per Year">
+                                <input type="text" name="Salary" id="Salary" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 Per Year" required>
                             </div>
+
+                            @error('Salary')
+                            <p class="text-xs text-red-500 font-semibold mt-1">{{$message}}</p>
+                            @enderror
                         </div>
                     </div>
+
+                    <!-- <div class="mt-10">
+                        @if($errors->any())
+                        <ul>
+                            @foreach($errors-all() as $error)
+                            <li class="text-red-500 italic">{{$error}}</li>
+                            @endforeach
+                        </ul>
+                        @endif
+                    </div> -->
                 </div>
             </div>
 
